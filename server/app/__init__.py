45005902eb8bd1fffdfc2b46ac0c92b51c2350fb
Sahil Patel <sahildepatel@gmail.com>
2025-01-22 10:34:32 -0500

    Setting up firestore

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from google.cloud import firestore
+from app.routes import plants, auth, ai
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('app.config.Config')
-    app.firestore = firestore.Client()
+    CORS(app)
+    # Initialize Firestore
+    db = firestore.Client.from_service_account_json('path/to/firebase_config.json')
+
+    # Register Blueprints
+    app.register_blueprint(plants.bp)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(ai.bp)
+
     return app
