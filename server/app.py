955498966724385b7d2dd2c1695feb02cfd79f0e
Sahil Patel <sahildepatel@gmail.com>
2025-01-29 18:19:43 -0500

    Tweaking firebase

--- server/app.py
+++ server/app.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
-from google.cloud import firestore
+from config.firebaseSetup import get_firestore_client  # Import Firestore client
 from app.routes import plants_bp, auth_bp, ai_bp
-from app import create_app
 from dotenv import load_dotenv
+import os
+
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     load_dotenv()
+    FIREBASE_KEY_PATH = os.getenv("FIREBASE_KEY_PATH")
     db = firestore.Client.from_service_account_json(FIREBASE_KEY_PATH)
+
+
+    db = get_firestore_client()  # Use existing Firestore instance
     
     app.register_blueprint(auth_bp)
     app.register_blueprint(plants_bp)
