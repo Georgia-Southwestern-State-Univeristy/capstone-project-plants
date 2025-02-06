682ffa40dc1727bd7666644f08436b427e8f7b22
Sahil Patel <sahildepatel@gmail.com>
2025-02-05 22:19:33 -0500

    Deleting old server files

--- server/app/config/openaiSetup.py
+++
@@ -1,9 +0,0 @@
-import openai
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-openai.api_key = OPENAI_API_KEY
--- server/app/schemas/disease.py
+++
@@ -1,6 +0,0 @@
-class DiseaseModel(BaseModel):
-    disease_id: str
-    plant_id: str  # Links to a plant
-    name: str
-    symptoms: str
-    treatment: str
