df008701f51b6586401d4e8218be2dcd74c642e7
Sahil Patel <sahildepatel@gmail.com>
2025-02-08 12:23:16 -0500

    Deleting backend

--- server/app/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from .auth_routes import auth_bp
-    from .plants_routes import plants_bp
-    from .ai_routes import ai_bp
-    from .openai_routes import openai_bp
-    from .user_routes import user_bp
-except ImportError as e:
-    print(f"Error importing routes: {e}")
