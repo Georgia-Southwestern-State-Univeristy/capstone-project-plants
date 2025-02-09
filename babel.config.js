39ac95fc63bee452fdb6d3ef56556fabcd75f14f
luxnonbinarycode <158427914+luxnonbinarycode@users.noreply.github.com>
2025-02-09 02:58:28 -0500

    update 5

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    ['@vue/cli-plugin-babel/preset', {
+      useBuiltIns: 'usage',
+      corejs: 3,
+      modules: 'auto'
+    }]
   ],
   plugins: [
-    "@babel/plugin-transform-modules-commonjs"
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-transform-runtime'
   ]
 }
