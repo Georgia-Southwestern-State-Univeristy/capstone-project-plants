6060650fa0528d3629c0409f1a9ccd39d3545974
luxnonbinarycode <158427914+luxnonbinarycode@users.noreply.github.com>
2025-02-20 02:17:09 -0500

    update 53

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const routes = [
   { path: '/login', name: 'Login', component: login, meta: { requiresGuest: true } },
   { path: '/register', name: 'Register', component: register, meta: { requiresGuest: true } },
   { path: '/chat', name: 'Chat', component: chat, /*meta: { requiresAuth: true }*/ }, 
-  { path: '/userprofile', name: 'Profile', component: () => import('@/views/userprofile.vue'), meta: { requiresAuth: true } }
+  { path: '/userprofile', name: 'Profile', component: () => import('@/views/userprofile.vue'), /*meta: { requiresAuth: true }*/ }
 ]
 
 const router = createRouter({
